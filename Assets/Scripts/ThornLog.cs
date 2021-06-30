677eabc26dfbfc2854a7e3ee2726241830ac1555
SungHoon <leesunghoon03@naver.com>
2021-06-30 11:43:03 +0900

    Add Thorn Log/Log VFX

---
+++ Assets/Scripts/ThornLog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornLog : MonoBehaviour
+{
+    private readonly float rotSpeed = 10f;
+
+    private void FixedUpdate()
+    {
+        RotateSelf();
+    }
+
+    private void RotateSelf()
+    {
+        transform.Rotate(transform.forward * rotSpeed, Space.World);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.transform.GetComponent<PlayerLife>().isDead = true;
+        }
+    }
+}
