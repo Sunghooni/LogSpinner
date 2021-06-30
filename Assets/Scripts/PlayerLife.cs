f0c97a243541eaacbcf68cb31975a069e8d98fe4
SungHoon <leesunghoon03@naver.com>
2021-06-30 16:30:57 +0900

    Player Broken VFX

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
+    public GameObject brokenParts;
     public bool isDead = false;
 
     private void Update()
@@ -16,6 +17,13 @@ public class PlayerLife : MonoBehaviour
 
     private void DeathMotion()
     {
+        brokenParts.transform.SetParent(null);
         gameObject.SetActive(false);
+
+        for (int i = 0; i < brokenParts.transform.childCount; i++)
+        {
+            GameObject part = brokenParts.transform.GetChild(i).gameObject;
+            part.SetActive(true);
+        }
     }
 }
