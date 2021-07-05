193eb74a2881709914d5d138c08d6f473e49bc57
SungHoon <leesunghoon03@naver.com>
2021-07-05 14:02:48 +0900

    Game Clear Effects

---
+++ Assets/Scripts/Tutorial/FinalLog.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalLog : MonoBehaviour
+{
+    private Log log;
+    private bool finishing = false;
+
+    private void Awake()
+    {
+        log = GetComponent<Log>();
+    }
+
+    private void Update()
+    {
+        if (IsFinished() && !finishing)
+        {
+            Invoke(nameof(LoadStageScene), 1f);
+            finishing = true;
+        }
+    }
+
+    private bool IsFinished()
+    {
+        if (log.isUsed)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void LoadStageScene()
+    {
+        FinishManager.instance.LoadScene("Tutorial");
+    }
+}
